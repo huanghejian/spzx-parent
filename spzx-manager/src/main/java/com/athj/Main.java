480749f3996530bab667388a67b69a73a3bf14a8
huanghejian <2243407455@qq.com>
2023-11-23 11:09:11 +0800

    提交

--- spzx-manager/src/main/java/com/athj/Main.java
+++ spzx-manager/src/main/java/com/athj/Main.java
@@ -1,11 +1,15 @@
-package com.athj;/**
-*ClassName:${NAME}
-*Package:com.athj
-*Description:
-*@Author:小飞机
-*@Create：${DATE} - ${TIME}
-*@Version: v1.0
-*/public class Main {
+package com.athj;
+
+/**
+ * ClassName:${NAME}
+ * Package:com.athj
+ * Description:
+ *
+ * @Author:小飞机
+ * @Create：${DATE} - ${TIME}
+ * @Version: v1.0
+ */
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
